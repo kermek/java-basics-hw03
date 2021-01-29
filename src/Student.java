b3694c9918cb545b3b577a51b4250ca2c2fdc921
Oleh Diliahin <sparq044@gmail.com>
2021-01-29 13:14:36 +0200

    homework commit

--- src/Student.java
+++ src/Student.java
@@ -2,15 +2,29 @@ public class Student {
   int rating;
   private String name;
 
-  // TODO implement Student class according to the instructions provided in the README.md file
+  public static int count = 0;
+  public static int totalRating = 0;
+
+  public Student() {
+    count++;
+  }
 
   public Student(String name) {
-    //TODO initialize name
+    this.name = name;
+    count++;
+  }
+
+  public Student(String name, int rating) {
+    this.name = name;
+    this.rating = rating;
+    count++;
+    totalRating += rating;
   }
 
   public static double getAvgRating() {
-    // TODO return average rating of all students
-    return 0;
+    double averageRating = 0.0;
+    if (count > 0) averageRating = totalRating / (double) count;
+    return averageRating;
   }
 
   public String getName() {
@@ -18,7 +32,7 @@ public class Student {
   }
 
   public void setName(String name) {
-    // TODO set student's name
+    this.name = name;
   }
 
   public int getRating() {
@@ -26,25 +40,40 @@ public class Student {
   }
 
   public void setRating(int rating) {
-    // TODO initialize rating;
+    this.rating = rating;
+    totalRating += rating;
   }
 
   public boolean betterStudent(Student student) {
-    // TODO return the result of comparing this.student's rating with the student's rating
-    return false;
+    return this.rating > student.rating;
   }
 
   public void changeRating(int rating) {
-    // TODO change this student's rating and average rating of all students
+    totalRating -= this.rating;
+    this.rating = rating;
+    setRating(rating);
   }
 
   public static void removeStudent(Student student) {
-    // TODO remove student
+    count--;
+    totalRating -= student.rating;
   }
 
   @Override
   public String toString() {
-    // TODO return String with name and rating of this student
-    return "";
+    return getName() + "" + getRating();
+  }
+
+  public static void main (String[] args) {
+    Student stud1 = new Student("Marlin", 22);
+    Student stud2 = new Student("Alex", 30);
+    Student stud3 = new Student("Kurt", 27);
+
+    System.out.println("Average students rating is: " + getAvgRating());
+    stud1.changeRating(24);
+    stud2.changeRating(23);
+    stud3.changeRating(45);
+
+    System.out.println("Average student rating is: " + getAvgRating());
   }
 }
