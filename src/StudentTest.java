8977c229a1ecbe36d5e15a3792065eddf935dc94
Alex Rybkin <30799266+alex-i-rybkin@users.noreply.github.com>
2022-02-10 16:23:13 +0200

    Update StudentTest.java
    Fix tests for getting score average if static field is used for sum

--- src/StudentTest.java
+++ src/StudentTest.java
@@ -4,140 +4,143 @@ import org.testng.annotations.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.testng.Assert.*;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
 
 public class StudentTest {
-  List<Student> students = new ArrayList<>();
+    List<Student> students = new ArrayList<>();
 
-  @Test
-  public void testGetAvgRating() {
 
-    assertEquals(Student.getAvgRating(), 0.0d, "Average rating is wrong");
+    @Test(priority = 1)
+    public void testGetAvgRatingNoStudents() {
+        assertEquals(Student.getAvgRating(), 0.0d, "Average rating is wrong");
+    }
 
-    Student student1 = new Student("Petro");
-    student1.setRating(15);
-    Student student2 = new Student("Volodymyr");
-    student2.setRating(30);
+    @Test(priority = 2)
+    public void testGetAvgRating() {
+        Student student1 = new Student("Petro");
+        student1.setRating(15);
+        Student student2 = new Student("Volodymyr");
+        student2.setRating(30);
 
-    students.add(student1);
-    students.add(student2);
+        students.add(student1);
+        students.add(student2);
 
-    assertEquals(Student.getAvgRating(), 22.5d, "Average rating is wrong");
-  }
+        assertEquals(Student.getAvgRating(), 22.5d, "Average rating is wrong");
+    }
 
-  @Test
-  public void testGetName() {
-    Student student = new Student("Petro");
+    @Test(priority = 2)
+    public void testGetName() {
+        Student student = new Student("Petro");
 
-    students.add(student);
+        students.add(student);
 
-    assertEquals(student.getName(), "Petro", "Student's name is wrong");
-  }
+        assertEquals(student.getName(), "Petro", "Student's name is wrong");
+    }
 
-  @Test
-  public void testSetName() {
-    Student student = new Student("Petro");
-    student.setName("Volodymyr");
+    @Test(priority = 2)
+    public void testSetName() {
+        Student student = new Student("Petro");
+        student.setName("Volodymyr");
 
-    students.add(student);
+        students.add(student);
 
-    assertEquals(student.getName(), "Volodymyr", "Student's name is wrong");
-  }
+        assertEquals(student.getName(), "Volodymyr", "Student's name is wrong");
+    }
 
-  @Test
-  public void testGetRating() {
-    Student student1 = new Student("Volodymyr");
+    @Test(priority = 2)
+    public void testGetRating() {
+        Student student1 = new Student("Volodymyr");
 
-    students.add(student1);
+        students.add(student1);
 
-    assertEquals(student1.getRating(), 0, "Student's name is wrong");
+        assertEquals(student1.getRating(), 0, "Student's name is wrong");
 
-    Student student2 = new Student("Petro");
-    student2.setRating(51);
+        Student student2 = new Student("Petro");
+        student2.setRating(51);
 
-    students.add(student2);
+        students.add(student2);
 
-    assertEquals(student2.getRating(), 51, "Student's name is wrong");
-  }
+        assertEquals(student2.getRating(), 51, "Student's name is wrong");
+    }
 
-  @Test
-  public void testSetRating() {
-    Student student = new Student("Volodymyr");
-    student.setRating(30);
+    @Test(priority = 2)
+    public void testSetRating() {
+        Student student = new Student("Volodymyr");
+        student.setRating(30);
 
-    students.add(student);
+        students.add(student);
 
-    assertEquals(student.getRating(), 30, "Student's name is wrong");
-  }
+        assertEquals(student.getRating(), 30, "Student's name is wrong");
+    }
 
-  @Test
-  public void testBetterStudent() {
-    Student student1 = new Student("Petro");
-    student1.setRating(15);
+    @Test(priority = 2)
+    public void testBetterStudent() {
+        Student student1 = new Student("Petro");
+        student1.setRating(15);
 
-    students.add(student1);
+        students.add(student1);
 
-    Student student2 = new Student("Volodymyr");
-    student2.setRating(30);
+        Student student2 = new Student("Volodymyr");
+        student2.setRating(30);
 
-    students.add(student2);
+        students.add(student2);
 
-    assertFalse(student1.betterStudent(student2),
-        student1 + " is not better than " + student2);
+        assertFalse(student1.betterStudent(student2),
+                student1 + " is not better than " + student2);
 
-    Student student3 = new Student("Petro");
-    student3.setRating(51);
+        Student student3 = new Student("Petro");
+        student3.setRating(51);
 
-    students.add(student3);
+        students.add(student3);
 
-    Student student4 = new Student("Volodymyr");
-    student4.setRating(30);
+        Student student4 = new Student("Volodymyr");
+        student4.setRating(30);
 
-    students.add(student4);
+        students.add(student4);
 
-    assertTrue(student3.betterStudent(student4),
-        student1 + " is not better than " + student2);
-  }
+        assertTrue(student3.betterStudent(student4),
+                student1 + " is not better than " + student2);
+    }
 
-  @Test
-  public void testChangeRating() {
+    @Test(priority = 2)
+    public void testChangeRating() {
 
-    assertEquals(Student.getAvgRating(), 0.0d, "Average rating is wrong");
+        Student student1 = new Student("Petro");
+        student1.setRating(15);
 
-    Student student1 = new Student("Petro");
-    student1.setRating(15);
+        students.add(student1);
 
-    students.add(student1);
+        Student student2 = new Student("Volodymyr");
+        student2.setRating(30);
 
-    Student student2 = new Student("Volodymyr");
-    student2.setRating(30);
+        students.add(student2);
 
-    students.add(student2);
+        assertEquals(Student.getAvgRating(), 22.5d, "Average rating is wrong");
 
-    assertEquals(Student.getAvgRating(), 22.5d, "Average rating is wrong");
+        student2.changeRating(51);
 
-    student2.changeRating(51);
+        assertEquals(Student.getAvgRating(), 33.0d, "Average rating is wrong");
+    }
 
-    assertEquals(Student.getAvgRating(), 33.0d, "Average rating is wrong");
-  }
+    @Test(priority = 2)
+    public void testToString() {
+        Student student = new Student("Petro");
+        student.setRating(51);
 
-  @Test
-  public void testToString() {
-    Student student = new Student("Petro");
-    student.setRating(51);
+        students.add(student);
 
-    students.add(student);
+        assertTrue(student.toString().contains("Petro"),
+                "toString method does not contains student's name");
 
-    assertTrue(student.toString().contains("Petro"),
-        "toString method does not contains student's name");
+        assertTrue(student.toString().contains("51"),
+                "toString method does not contains student's rating");
+    }
 
-    assertTrue(student.toString().contains("51"),
-        "toString method does not contains student's rating");
-  }
-
-  @AfterMethod
-  public void tearDown() {
-    students.forEach(Student :: removeStudent);
-    students.clear();
-  }
+    @AfterMethod
+    public void tearDown() {
+        students.forEach(Student::removeStudent);
+        students.clear();
+    }
 }
